a5adf78870959c8d664b3d73cfb859bb8121e29f
AswanthAnu <aswanthanu777@gmail.com>
2023-09-27 10:36:45 +0530

    somechanges

--- localmarket/api/serializers.py
+++ localmarket/api/serializers.py
@@ -4,12 +4,14 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'first_name', 'last_name' 'password']
+        fields = ['username', 'first_name', 'last_name', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser(
             username=validated_data['username'],
+            first_name=validated_data["first_name"],
+            last_name=validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
